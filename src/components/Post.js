afef3e5a439536492f5bf4b005c9e57c09637ad5
natdanaiw08 <natdanaiwangluang@gmail.com>
2020-05-10 02:18:36 +0700

    update post

--- src/components/Post.js
+++ src/components/Post.js
@@ -28,33 +28,35 @@ const Post = ({ data, slug, pageContext, location }) => {
     const [html, setHtml] = useState("");
     useEffect(() => {
         var html = "";
-        post.html.blocks.forEach(function (block) {
-            switch (block.type) {
-                case 'header':
-                    html += `<h${block.data.level}>${block.data.text}</h${block.data.level}>`;
-                    break;
-                case 'paragraph':
-                    html += `<p>${block.data.text}</p>`;
-                    break;
-                case 'delimiter':
-                    html += '<hr />';
-                    break;
-                case 'image':
-                    html += `<img class="img-fluid" src="${block.data.file.url}" title="${block.data.caption}" /><br /><em>${block.data.caption}</em>`;
-                    break;
-                case 'list':
-                    html += '<ul>';
-                    block.data.items.forEach(function (li) {
-                        html += `<li>${li}</li>`;
-                    });
-                    html += '</ul>';
-                    break;
-                default:
-                    console.log('Unknown block type', block.type);
-                    break;
-            }
-        })
-        setHtml(html);
+        if (post.html) {
+            post.html.blocks.forEach(function (block) {
+                switch (block.type) {
+                    case 'header':
+                        html += `<h${block.data.level}>${block.data.text}</h${block.data.level}>`;
+                        break;
+                    case 'paragraph':
+                        html += `<p>${block.data.text}</p>`;
+                        break;
+                    case 'delimiter':
+                        html += '<hr />';
+                        break;
+                    case 'image':
+                        html += `<img class="img-fluid" src="${block.data.file.url}" title="${block.data.caption}" /><br /><em>${block.data.caption}</em>`;
+                        break;
+                    case 'list':
+                        html += '<ul>';
+                        block.data.items.forEach(function (li) {
+                            html += `<li>${li}</li>`;
+                        });
+                        html += '</ul>';
+                        break;
+                    default:
+                        console.log('Unknown block type', block.type);
+                        break;
+                }
+            })
+            setHtml(html);
+        }
     }, [post]);
 
     return (
