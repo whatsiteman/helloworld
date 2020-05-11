cf4d615f162cf765e490b1b53e94562e4b05171d
natdanaiw08 <natdanaiwangluang@gmail.com>
2020-05-11 13:17:45 +0700

    update try node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,56 +2,56 @@ const path = require(`path`);
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const result = await graphql(`
-    query {
-      allSetting {
-        edges {
-          node {
-            key
-            value
+  try {
+    const result = await graphql(`
+      query {
+        allSetting {
+          edges {
+            node {
+              key
+              value
+            }
+          }
+        }
+        allPost(sort: { order: DESC }, limit: 1000) {
+          edges {
+            node {
+              title
+              slug
+              excerpt
+              image
+            }
           }
         }
       }
-      allPost(sort: { order: DESC }, limit: 1000) {
-        edges {
-          node {
-            title
-            slug
-            excerpt
-            image
-          }
-        }
-      }
-    }
-  `).catch((err) => {
-    console.error(err);
-  });
+    `);
 
-  if (result.data) {
-    const postTemplate = path.resolve(`./src/templates/Post.js`);
-    // Create blog posts pages.
-    const settings = result.data.allSetting.edges.reduce(
-      (a, x) => ({ ...a, [x.node.key]: x.node.value }),
-      {}
-    );
-    const posts = result.data.allPost.edges;
-    posts.forEach(({ node }, index) => {
-      const previous =
-        index === posts.length - 1 ? null : posts[index + 1].node;
-      const next = index === 0 ? null : posts[index - 1].node;
+    if (result.data) {
+      const postTemplate = path.resolve(`./src/templates/Post.js`);
+      // Create blog posts pages.
+      const settings = result.data.allSetting.edges.reduce(
+        (a, x) => ({ ...a, [x.node.key]: x.node.value }),
+        {}
+      );
+      const posts = result.data.allPost.edges;
+      posts.forEach(({ node }, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node;
+        const next = index === 0 ? null : posts[index - 1].node;
 
-      createPage({
-        path: "/posts/" + node.slug,
-        component: postTemplate,
-        context: {
-          settings,
-          slug: node.slug,
-          previous,
-          next,
-        },
+        createPage({
+          path: "/posts/" + node.slug,
+          component: postTemplate,
+          context: {
+            settings,
+            slug: node.slug,
+            previous,
+            next,
+          },
+        });
       });
-    });
-  }
+    }
+  } catch (err) {}
 };
 
 exports.onCreatePage = ({ page, actions }) => {
