9781ee1feb268b85eb7a0c2fb8c18684879bb359
natdanaiw08 <natdanaiwangluang@gmail.com>
2020-05-11 12:25:00 +0700

    update node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,65 +1,63 @@
 const path = require(`path`);
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const postTemplate = path.resolve(`./src/templates/Post.js`);
-  const result = await graphql(`
-    query {
-      allSetting {
-        edges {
-          node {
-            key
-            value
-          }
-        }
-      }
-      allPost(sort: { order: DESC }, limit: 1000) {
-        edges {
-          node {
-            title
-            slug
-            excerpt
-            image
-          }
-        }
-      }
-    }
-  `);
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const postTemplate = path.resolve(`./src/templates/Post.js`);
+//   const result = await graphql(`
+//     query {
+//       allSetting {
+//         edges {
+//           node {
+//             key
+//             value
+//           }
+//         }
+//       }
+//       allPost(sort: { order: DESC }, limit: 1000) {
+//         edges {
+//           node {
+//             title
+//             slug
+//             excerpt
+//             image
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  if (result.errors) {
-    return false;
-  }
-  // Create blog posts pages.
-  const settings = result.data.allSetting.edges.reduce(
-    (a, x) => ({ ...a, [x.node.key]: x.node.value }),
-    {}
-  );
-  const posts = result.data.allPost.edges;
-  posts.forEach(({ node }, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
+//   if (result.errors) {
+//     return false;
+//   }
+//   // Create blog posts pages.
+//   const settings = result.data.allSetting.edges.reduce(
+//     (a, x) => ({ ...a, [x.node.key]: x.node.value }),
+//     {}
+//   );
+//   const posts = result.data.allPost.edges;
+//   posts.forEach(({ node }, index) => {
+//     const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+//     const next = index === 0 ? null : posts[index - 1].node;
 
-    createPage({
-      path: "/posts/" + node.slug,
-      component: postTemplate,
-      context: {
-        settings,
-        slug: node.slug,
-        previous,
-        next,
-      },
-    });
-  });
-};
+//     createPage({
+//       path: "/posts/" + node.slug,
+//       component: postTemplate,
+//       context: {
+//         settings,
+//         slug: node.slug,
+//         previous,
+//         next,
+//       },
+//     });
+//   });
+// };
 
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
-  // Make the front page match everything client side.
-  // Normally your paths should be a bit more judicious.
-  // if (page.path === `/`) {
-  //   page.matchPath = `/*`;
-  //   createPage(page);
-  // }
+  if (page.path.match(/^\/preview\/helloworld\/posts/)) {
+    page.matchPath = `/preview/helloworld/posts/*`;
+    createPage(page);
+  }
 };
 
 exports.createSchemaCustomization = ({ actions }) => {
